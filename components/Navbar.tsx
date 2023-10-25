8abcfd72ad989eeefc0fe1c9bceaa6a6a583eb52
IsmailDX <ismaildxx@gmail.com>
2023-10-26 00:22:04 +0400

    fixed nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,31 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Icon from "../public/hero/Menu_icon.png";
-import { VscChevronDown } from "react-icons/vsc";
+import { VscChevronDown, VscChevronUp } from "react-icons/vsc";
 
 const sidebar = ["HOME", "SERVICES", "WORK", "CREW", "CONTACT"];
+const workItems = ["PRODUCTION", "DIGITAL", "CREATIVE", "STRATEGY"];
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [showWorkItems, setShowWorkItems] = useState(false);
+  const [workIcon, setWorkIcon] = useState(false);
+
   const handleClick = () => {
     setOpen(!open);
   };
 
+  const toggleWorkItems = () => {
+    setShowWorkItems(!showWorkItems);
+    setWorkIcon(!showWorkItems);
+  };
+
   return (
     <>
-      <div className="lg:w-[80%] lg:pl-44 lg:px-0 flex justify-between lg:items-end py-6 w-full sm:px-10 px-3 items-center relative">
+      <div
+        className="lg:w-[80%] lg:pl-44 lg:px-0 flex justify-between lg:items-end py-6
+       w-full sm:px-10 px-3 items-center relative overflow-hidden bg-transparent"
+      >
         <h1 className="text-4xl tracking-widest font-semibold">ASTUDIO</h1>
         <p className="hidden lg:block">START A PROJECT</p>
         <Image
@@ -24,27 +36,46 @@ const Navbar = () => {
           onClick={handleClick}
         />
       </div>
-      {!open ? (
-        <div
-          className={`absolute top-13 right-0 flex flex-col p-12 border-black lg:hidden
-        ${!open ? "border-t" : "border-t-0"}`}
-        >
-          <ul className="w-full space-y-4 pb-7 pl-4">
-            {sidebar.map((item) => (
-              <li key={item} className="text-lg font-medium">
-                {item === "WORK" ? (
-                  <span className="flex items-center gap-2">
-                    {item} <VscChevronDown />
-                  </span>
-                ) : (
-                  item
-                )}
-              </li>
-            ))}
-          </ul>
-          <button className="border-black">Start a project</button>
-        </div>
-      ) : null}
+
+      {/* Mobile Nav */}
+
+      <div
+        className={`absolute flex flex-col p-12 right-0 border-black lg:hidden
+        ${
+          !open
+            ? "border-t top-13 opacity-100 z-9999"
+            : "border-t-0 opacity-0 z-0"
+        } transition-all ease-in-out duration-200 bg-white`}
+      >
+        <ul className="w-full space-y-4 pb-7 pl-4">
+          {sidebar.map((item) => (
+            <li key={item} className="text-lg font-medium">
+              {item === "WORK" ? (
+                <span className="flex items-center gap-2">
+                  {item}
+                  {workIcon ? (
+                    <VscChevronUp onClick={toggleWorkItems} />
+                  ) : (
+                    <VscChevronDown onClick={toggleWorkItems} />
+                  )}
+                </span>
+              ) : (
+                item
+              )}
+              {item === "WORK" && showWorkItems && (
+                <ul className="space-y-4 pt-4">
+                  {workItems.map((workitem) => (
+                    <li key={workitem} className="font-light pl-6">
+                      {workitem}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+        <button className="border-black">Start a project</button>
+      </div>
     </>
   );
 };
