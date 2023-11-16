b45aa34aab734a8a03701f8c45256ec9ae69cbed
IsmailDX <ismaildxx@gmail.com>
2023-11-16 19:15:27 +0400

    added react count up animation

--- components/Awards.tsx
+++ components/Awards.tsx
@@ -1,6 +1,39 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import Countup from "react-countup";
 
 const Awards = () => {
+  const [counterOn, setCounterOn] = useState(false);
+  const countRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const handleIntersection = (entries: any[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setCounterOn(true);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    if (countRef.current) {
+      observer.observe(countRef.current);
+    }
+
+    return () => {
+      if (countRef.current) {
+        observer.unobserve(countRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="w-full overflow-hidden">
       <div className="w-full h-full bg-red-700"></div>
@@ -21,12 +54,19 @@ const Awards = () => {
           </div>
 
           <div className="max-w-[100%] flex flex-row pt-16 gap-6 justify-around lg:pl-0 pl-11">
-            <div className="flex flex-col w-fit -space-y-20 items-start">
+            <div
+              ref={countRef}
+              className="flex flex-col w-fit -space-y-20 items-start"
+            >
               <h4 className="lg:text-[20px] text-[15px] text-primary underline whitespace-nowrap">
                 WORLDWIDE AWARDS
               </h4>
               <h1 className="lg:text-[200px] text-[150px] text-white font-neutraface">
-                12
+                {counterOn ? (
+                  <Countup start={0} end={12} duration={2} delay={0} />
+                ) : (
+                  0
+                )}
               </h1>
             </div>
 
@@ -35,7 +75,11 @@ const Awards = () => {
                 TRUSTED YEARS
               </h4>
               <h1 className="lg:text-[200px] text-[150px] text-white font-neutraface">
-                14
+                {counterOn ? (
+                  <Countup start={0} end={14} duration={2} delay={0} />
+                ) : (
+                  0
+                )}
               </h1>
             </div>
 
@@ -44,7 +88,11 @@ const Awards = () => {
                 EMPLOYEES
               </h4>
               <h1 className="lg:text-[200px] text-[150px] text-white font-neutraface">
-                50
+                {counterOn ? (
+                  <Countup start={0} end={50} duration={2} delay={0} />
+                ) : (
+                  0
+                )}
               </h1>
             </div>
           </div>
@@ -59,7 +107,7 @@ const Awards = () => {
 
       {/* Mobile Awards */}
       <div className="flex flex-row w-full items-center overflow-hidden relative md:hidden">
-        <div className="div-3 overflow-hidden pt-28 w-full">
+        <div ref={countRef} className="div-3 overflow-hidden pt-28 w-full">
           <div className="flex flex-col w-full px-3">
             <h1
               className="text-5xl text-primary 
@@ -82,21 +130,39 @@ const Awards = () => {
               <h4 className="text-[11px] text-primary underline whitespace-nowrap line-clamp-2">
                 WORLDWIDE AWARDS
               </h4>
-              <h1 className="text-[90px] text-white font-neutraface">12</h1>
+              <h1 className="text-[90px] text-white font-neutraface">
+                {counterOn ? (
+                  <Countup start={0} end={12} duration={2} delay={0} />
+                ) : (
+                  0
+                )}
+              </h1>
             </div>
 
             <div className="flex flex-col w-fit -space-y-10 items-center">
               <h4 className="text-[11px] text-primary underline whitespace-nowrap">
                 TRUSTED YEARS
               </h4>
-              <h1 className="text-[90px] text-white font-neutraface">14</h1>
+              <h1 className="text-[90px] text-white font-neutraface">
+                {counterOn ? (
+                  <Countup start={0} end={14} duration={2} delay={0} />
+                ) : (
+                  0
+                )}
+              </h1>
             </div>
 
             <div className="flex flex-col w-fit -space-y-10 items-center">
               <h4 className="text-[11px] text-primary underline whitespace-nowrap">
                 EMPLOYEES
               </h4>
-              <h1 className="text-[90px] text-white font-neutraface">50</h1>
+              <h1 className="text-[90px] text-white font-neutraface">
+                {counterOn ? (
+                  <Countup start={0} end={50} duration={2} delay={0} />
+                ) : (
+                  0
+                )}
+              </h1>
             </div>
           </div>
         </div>
--- components/Cases.tsx
+++ components/Cases.tsx
@@ -6,30 +6,35 @@ const Cases = () => {
     <div className="w-full lg:px-[172px] md:px-[102px] mx-auto px-5 py-24">
       <h1 className="font-neutraface text-3xl text-primary pb-11">USE CASES</h1>
       <div className="w-full flex md:flex-row flex-col md:items-end justify-center items-start md:gap-12 gap-6">
-        <div className="pb-11">
-          <div className="relative">
-            <img
-              src="/cases/one.png"
-              alt="one"
-              className="md:max-w-full max-w-[300px]"
-            />
-            <button className="absolute left-0 bottom-0 fancyButton lg:w-fit lg:p-3 w-12 p-2">
-              <IoIosArrowRoundForward size={30} className="relative" />
-            </button>
-          </div>
-          <div className="w-full flex lg:flex-row flex-col justify-between pt-5">
-            <h2 className="uppercase text-primary text-lg font-neutraface">
-              Pandora — dana wolley zayat
-            </h2>
-            <p className="text-sm pt-1">Strategy</p>
-          </div>
-          <div className="max-w-md text-gray-500">
-            <p>
-              As a one-stop shop, we combine the creativity and storytelling of
-              an ad agency with the technical expertise and execution of a
-              production comany. We create brand stories through strategy and
-              market research.
-            </p>
+        <div
+          className="delay-[300ms] duration-[600ms] taos:translate-x-[-200px] taos:opacity-0"
+          data-taos-offset="400"
+        >
+          <div className="pb-11">
+            <div className="relative">
+              <img
+                src="/cases/one.png"
+                alt="one"
+                className="md:max-w-full max-w-[300px]"
+              />
+              <button className="absolute left-0 bottom-0 fancyButton lg:w-fit lg:p-3 w-12 p-2">
+                <IoIosArrowRoundForward size={30} className="relative" />
+              </button>
+            </div>
+            <div className="w-full flex lg:flex-row flex-col justify-between pt-5">
+              <h2 className="uppercase text-primary text-lg font-neutraface">
+                Pandora — dana wolley zayat
+              </h2>
+              <p className="text-sm pt-1">Strategy</p>
+            </div>
+            <div className="max-w-md text-gray-500">
+              <p>
+                As a one-stop shop, we combine the creativity and storytelling
+                of an ad agency with the technical expertise and execution of a
+                production comany. We create brand stories through strategy and
+                market research.
+              </p>
+            </div>
           </div>
         </div>
 
